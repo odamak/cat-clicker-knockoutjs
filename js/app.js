20a5c148db9a567f03bfab03cca61ce7b1df8119
omar <omardamak1@gmail.com>
2018-01-27 12:39:27 +0000

    Add cat level but bug when display list of array as nicknames

--- js/app.js
+++ js/app.js
@@ -4,9 +4,21 @@ var ViewModel = function() {
     this.imgSrc = ko.observable('img/cat_picture0.jpeg');
     this.imgAttribution = ko.observable('https://www.flickr.com/photos');
 
+    this.nicknames = ko.observableArray ([  {nickname: 'titi'},
+                                            {nickname: 'toutou'},
+                                            {nickname: 'tata'},
+                                            {nickname: 'tutu'},
+                                            {nickname: 'tonton'}]);
+
     this.incrementCounter = function() {
         this.clickCount(this.clickCount() + 1);
     };
+
+    this.level = ko.computed(function(){
+        return (this.clickCount() < 3 ? 'infant': 
+        (this.clickCount() < 6 ? 'teenage': 
+        (this.clickCount() < 11 ? 'adult': 'senior')))
+    },this)
 
 }
 
